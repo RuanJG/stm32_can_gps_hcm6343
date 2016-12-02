539b81aa5ad949c2f98f83e918359ef8ac75989f
RuanJG <RuanJG@gmail.com>
2016-12-02 15:30:27 +0800

    Ruan: add yunzhou logo and little modify in ui module

--- Project/YunZhouNewRemoter/Core/NewApp/PictureResource.h
+++ Project/YunZhouNewRemoter/Core/NewApp/PictureResource.h
@@ -5,3 +5,6 @@ extern const unsigned char _logoPic[2816];
 
 // ¸ÐÌ¾·ûºÅÍ¼°¸
 extern const unsigned char _warningPic[4856];
+
+extern unsigned char _logo_big[14454];//[22776];
+
--- Project/YunZhouNewRemoter/Core/NewApp/remoter_sender_UI.c
+++ Project/YunZhouNewRemoter/Core/NewApp/remoter_sender_UI.c
@@ -49,7 +49,18 @@ void _control_backlight( unsigned char on1off0)
 		bsp_lcd_backlight_pwm_set_level(0);//0%
 }
 
-
+void _ui_check_layer(int num)
+{
+	if( _ui_last_layer != num )
+	{
+		GUI_SelectLayer(_ui_last_layer);
+		GUI_Clear();	
+		GUI_SetColor(GUI_TRANSPARENT);
+		GUI_SetBkColor(GUI_TRANSPARENT);
+	}
+	_ui_last_layer = num;
+	GUI_SelectLayer(num);
+}
 
 
 
@@ -63,16 +74,10 @@ void show_warnning_setting(void)
 {
 
 	//clear 
-	if( _ui_last_layer != 1 )
-	{
-		GUI_SelectLayer(_ui_last_layer);
-		GUI_Clear();	
-		GUI_SetColor(GUI_TRANSPARENT);
-		GUI_SetBkColor(GUI_TRANSPARENT);
-	}
-	_ui_last_layer = 1;
+	_ui_check_layer(1);
 		
-	GUI_SelectLayer(1);
+	
+	//start draw
 	GUI_MEMDEV_Select(layer1_dialog_memdev);	
 	GUI_MEMDEV_Clear(layer1_dialog_memdev);	
 	GUI_SetColor(GUI_TRANSPARENT);		
@@ -90,70 +95,35 @@ void show_warnning_setting(void)
 }
 
 
-void show_jostick_status()
+void show_logo_and_initing(void)
 {
+
+	static int counter = 0;
+	int logo_display_Ms = 0;
+	GUI_MEMDEV_Handle logo_memdev;
 	
 	//clear 
-	if( _ui_last_layer != 0 )
+	_ui_check_layer(1);
+		
+	logo_memdev = GUI_MEMDEV_Create(0, 0, 200, 200);
+
+	GUI_MEMDEV_Clear(logo_memdev);		
+	GUI_MEMDEV_Select(logo_memdev );
+	GUI_SetBkColor(GUI_TRANSPARENT);
+	GUI_SetColor(GUI_TRANSPARENT);	
+	GUI_BMP_DrawScaled(_logo_big,180,100,2,1);
+	GUI_MEMDEV_CopyToLCDAt(logo_memdev, 0, 0);
+
+	
+	while( remoter_sender_RF_getMode() != _RF_NORMAL_MODE  && logo_display_Ms< 4000 )
 	{
-		GUI_SelectLayer(_ui_last_layer);
-		GUI_Clear();	
-		GUI_SetColor(GUI_TRANSPARENT);
-		GUI_SetBkColor(GUI_TRANSPARENT);
+		vTaskDelay(20);
+		logo_display_Ms += 20;
 	}
-	_ui_last_layer = 0;
-	
-	GUI_SelectLayer(0);
-	GUI_MEMDEV_Select(layer0_textbox_memdev);	
-			
-// start draw 
-	GUI_SetBkColor(GUI_TRANSPARENT);		
-	GUI_SetTextAlign(GUI_TA_LEFT | GUI_TA_VCENTER);
-	GUI_SetFont(&GUI_Font8x16_ASCII);
-	GUI_SetColor(GUI_WHITE);
-	
-	
-	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	GUI_DispStringAt("mode  ", 0, 20);GUI_DispDec(jostick->key_mode,1);
-	GUI_DispStringAt("sample  ", 0, 40);GUI_DispDec(jostick->key_sample,1);
-	GUI_DispStringAt("alarm  ", 0, 60);GUI_DispDec(jostick->key_alarm,1);
-	GUI_DispStringAt("Left-x  ", 0, 80);GUI_DispDec(jostick->left_x,4);
-	GUI_DispStringAt("Left-y  ", 0, 100);GUI_DispDec(jostick->left_y,4);
-	GUI_DispStringAt("Right-x  ", 0, 120);GUI_DispDec(jostick->right_x,4);
-	GUI_DispStringAt("Right-y  ", 0, 140);GUI_DispDec(jostick->right_y,4);
-	GUI_DispStringAt("Knob  ", 0, 160);GUI_DispDec(jostick->knob,4);
-	GUI_DispStringAt("Power  ", 0, 180);GUI_DispDec(bsp_Get_Battery_Level(),4);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 250, 0);
-	
-	
-	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	GUI_DispStringAt("boat_power   ", 0, 20);GUI_DispDec(remoter_sender_RF_get_boat_powerLevel(),4);
-	GUI_DispStringAt("channel  ",0 , 50);GUI_DispDec(rf_config->hp,4);
-	GUI_DispStringAt("xtend_id  ",0 , 65);GUI_DispDec(rf_config->id,8);
-	GUI_DispStringAt("RSSI  ",0 , 80);GUI_DispDec(rf_config->id,8);
-	GUI_DispStringAt("decode_rate  ",0 , 95);GUI_DispDec(rf_config->id,8);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 50, 0);
-	
-	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	GUI_DispStringAt("log:",0 , 10);
-	sprintf(_log_buffer,"xtend_id=%04x\n",rf_config->id);
-	GUI_DispStringAt(_log_buffer,0,30);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 20, 150);
-	
+	GUI_MEMDEV_Delete(logo_memdev);
 }
 
-void _ui_check_layer()
-{
-	if( _ui_last_layer != 0 )
-	{
-		GUI_SelectLayer(_ui_last_layer);
-		GUI_Clear();	
-		GUI_SetColor(GUI_TRANSPARENT);
-		GUI_SetBkColor(GUI_TRANSPARENT);
-	}
-	_ui_last_layer = 0;
-	GUI_SelectLayer(0);
-}
+
 
 void show_jostick_status_text()
 {
@@ -161,7 +131,7 @@ void show_jostick_status_text()
 	char strBuffer[128];
 	
 	//clear 
-	_ui_check_layer();
+	_ui_check_layer(0);
 			
 // start draw 
 	GUI_SetBkColor(GUI_TRANSPARENT);		
@@ -172,29 +142,52 @@ void show_jostick_status_text()
 	GUI_MEMDEV_Select(layer0_textbox_memdev);	
 	
 	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	x= 0; y=20;
-	GUI_DispStringAt("mode     ", x, y);GUI_DispDec(jostick->key_mode,1);
-	GUI_DispStringAt("sample   ", x, y+20);GUI_DispDec(jostick->key_sample,1);
-	GUI_DispStringAt("alarm    ", x, y+40);GUI_DispDec(jostick->key_alarm,1);
-	GUI_DispStringAt("Left-x   ", x, y+60);GUI_DispDec(jostick->left_x,4);
-	GUI_DispStringAt("Left-y   ", x, y+80);GUI_DispDec(jostick->left_y,4);
-	GUI_DispStringAt("Right-x  ", x, y+100);GUI_DispDec(jostick->right_x,4);
-	GUI_DispStringAt("Right-y  ", x, y+120);GUI_DispDec(jostick->right_y,4);
-	GUI_DispStringAt("Knob     ", x, y+140);GUI_DispDec(jostick->knob,4);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 40, 20);
+	x= 0; y=10;
+	GUI_DispStringAt("------ Jostick -----",x , y); 
+	
+	y+=15; 
+	sprintf(strBuffer,"mode-switch      %d   ",jostick->key_mode);  GUI_DispStringAt(strBuffer, x, y);
+	sprintf(strBuffer,"sample-switch    %d   ",jostick->key_sample);  GUI_DispStringAt(strBuffer, x, y+20);
+	sprintf(strBuffer,"alarm-switch     %d   ",jostick->key_alarm);  GUI_DispStringAt(strBuffer, x, y+40);
+	
+	y+=10;
+	sprintf(strBuffer,"Left-x   	%d   ",(jostick->left_x/100-20)*5 );  GUI_DispStringAt(strBuffer, x, y+60);
+	sprintf(strBuffer,"Left-y   	%d   ",(jostick->left_y/100-20)*5 );  GUI_DispStringAt(strBuffer, x, y+80);
+	sprintf(strBuffer,"Right-x  	%d   ",(jostick->right_x/100-20)*5 );  GUI_DispStringAt(strBuffer, x, y+100);
+	sprintf(strBuffer,"Right-y  	%d   ",(jostick->right_y/100-20)*5 );  GUI_DispStringAt(strBuffer, x, y+120);
+	sprintf(strBuffer,"Knob     	%d   ",(jostick->knob/10)*100/409 );  GUI_DispStringAt(strBuffer, x, y+140);
+	
+	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 40, 10);
+	
+	
+	GUI_MEMDEV_Clear(layer0_textbox_memdev);
+	x = 0; y = 10;
+	GUI_DispStringAt("------- Power ------", x, y);
+	sprintf(strBuffer,"Remoter     %d    ",bsp_Get_Battery_Level());
+	GUI_DispStringAt(strBuffer, x, y+15);
+	sprintf(strBuffer,"Boat        %d    ",remoter_sender_RF_get_boat_powerLevel());
+	GUI_DispStringAt(strBuffer, x, y+30);
+
+	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 40, 200);
 	
 
+	x = 0; y= 0;
+	GUI_MEMDEV_Clear(layer0_textbox_memdev);
+	GUI_BMP_Draw(_logo_big, x, y+5);
+	//GUI_BMP_DrawScaled(_logo_big, x, y+10, 3, 2);
+	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 300, 10);
+	
 	
 	GUI_MEMDEV_Clear(layer0_textbox_memdev);
 	x = 0; y = 20;
-	GUI_DispStringAt("------ Wireless ---------", x, y);
-	sprintf(strBuffer,"Channel      %d",rf_config->hp);
+	GUI_DispStringAt("------ Radio  -------", x, y);
+	sprintf(strBuffer,"Channel      %d    ",rf_config->hp);
 	GUI_DispStringAt(strBuffer ,x , y+15);
-	sprintf(strBuffer,"xtend_id     %d",rf_config->id);
+	sprintf(strBuffer,"xtend_id     %d    ",rf_config->id);
 	GUI_DispStringAt(strBuffer ,x , y+30);
-	sprintf(strBuffer,"RSSI         %d -- %d ",xtend900_get_rssi(),xtend900_get_rssi_level() );
+	sprintf(strBuffer,"RSSI         %d -- %d     ",xtend900_get_rssi(),xtend900_get_rssi_level() );
 	GUI_DispStringAt(strBuffer,x , y+45);
-	sprintf(strBuffer,"decode_rate  %d",remoter_sender_RF_getDecodeRate() );
+	sprintf(strBuffer,"decode_rate  %d    ",remoter_sender_RF_getDecodeRate() );
 	GUI_DispStringAt(strBuffer,x , y+60);
 	GUI_DispStringAt("Status       ",x , y+75);
 	switch(remoter_sender_RF_getMode())
@@ -219,76 +212,37 @@ void show_jostick_status_text()
 		default:
 			break;
 	}
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 200, 20);
+	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 250, 80);
 
 	
-	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	x = 0; y = 20;
-	GUI_DispStringAt("------ Power --------", x, y);
-	sprintf(strBuffer,"Remoter   %d",bsp_Get_Battery_Level());
-	GUI_DispStringAt(strBuffer, x, y+20);
-	sprintf(strBuffer,"Boat      %d",remoter_sender_RF_get_boat_powerLevel());
-	GUI_DispStringAt(strBuffer, x, y+40);
-	sprintf(strBuffer,"Cpu       %d",FreeRTOS_GetCPUUsage());
-	GUI_DispStringAt(strBuffer, x, y+60);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 200, 120);
+
 	
-	x = 0; y= 20;
+	x = 0; y= 10;
 	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	GUI_DispStringAt("------ Log   ---------",x , y);
-	GUI_DispStringAt(" ",x, y+15);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 200, 200);
+	GUI_DispStringAt("------ Log    -------",x , y);
+	sprintf(strBuffer,"Cpu Usage   %d   ",FreeRTOS_GetCPUUsage());
+	GUI_DispStringAt(strBuffer, x, y+15);
+	GUI_DispStringAt(_log_buffer,x, y+30);
+	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 250, 200);
+	
+
 }
 
 
+
+
+
+
+
+
+//just for test
 void remoter_sender_UI_setLog(char *str)
 {
-	int	x = 0, y= 20;
-	GUI_MEMDEV_Clear(layer0_textbox_memdev);
-	GUI_DispStringAt(_log_buffer,x, y+15);
-	GUI_MEMDEV_CopyToLCDAt(layer0_textbox_memdev, 200, 200);
+	sprintf(_log_buffer,"%s",str);
 }
 
 
-void do_test()
-{
-	static int count=50;
-	static int last_status=0;
-	
-#if 0
-	if(jostick->left_y>3000)count=916;           //do
-	else if(jostick->left_y<1000)count=946;      //re
-	else if(jostick->left_x<1000)count=976;      //mi
-	else if(jostick->right_y>3000)count=1006;    //fa
-	else if(jostick->right_y<1000)count=1036;    //so
-	else if(jostick->right_x>3000)count=1076;    //la
-	else if(jostick->right_x<1000)count=1106;    //xi
-	last_status = jostick->left_y>3000 | jostick->left_y<1000 | jostick->left_x<1000 
-             | jostick->right_y>3000 | jostick->right_y<1000 | jostick->right_x>3000 
-						 | jostick->right_x<1000;
-	 if(last_status==1)	bsp_buzzer_pwm_set_voice(count,50);
-	else bsp_buzzer_pwm_set_voice(count,0);
-#endif
-	
-	if( last_status== 0 &&jostick->button_ok == 1  )
-	{
-		count+=10;
-		//bsp_buzzer_pwm_set_level(count);
-		//bsp_buzzer_pwm_set_voice(count,50);
-		bsp_lcd_backlight_pwm_set_level(count);
-		printf("light level=%d\r\n",count);
-	}
-	if( last_status== 0 &&jostick->button_cancel == 1  )
-	{
-		count-=10;
-		//bsp_buzzer_pwm_set_level(count);
-		//bsp_buzzer_pwm_set_voice(count,50);
-		bsp_lcd_backlight_pwm_set_level(count);
-		printf("light level=%d\r\n",count);
-	}
-  last_status = jostick->button_ok | jostick->button_cancel ;
 
-}
 
 void remoter_sender_UI_Task(void * param)
 {
@@ -298,7 +252,7 @@ void remoter_sender_UI_Task(void * param)
 	
 	
 	/* Initialize the BSP layer */
-  bsp_Gui_lcd_init();
+	bsp_Gui_lcd_init();
  
 	//init 
 	GUI_Init();
@@ -324,23 +278,15 @@ void remoter_sender_UI_Task(void * param)
 	jostick = remoter_sender_jostick_get_data();
 	rf_config = remoter_sender_RF_get_config();
 	
+	//loading param , so display logo now 
+	//show_logo_and_initing();
+	
 	while(1)
 	{	
-		/*
-		if( _RF_SETTING_MODE == remoter_sender_RF_getMode() )
-		{
-			current_win_id = 1;
-		}else{
-			current_win_id = 0;
-		}*/
-		
-		
 		switch( current_win_id )
 		{
 			case 0:
 			{
-				
-				//show_jostick_status();
 				show_jostick_status_text();
 				break;
 			}
@@ -352,8 +298,6 @@ void remoter_sender_UI_Task(void * param)
 		}
 
 		vTaskDelay(20);
-		
-		//do_test();
 		
 	}
 	
